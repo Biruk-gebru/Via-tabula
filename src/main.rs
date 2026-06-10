2b31bfbb3d87fc85f26620a2cbb1adbd24cfff3a
Biruk-gebru <birukjember2004@gmail.com>
2026-06-10 22:33:22 +0300

    chore(main): add MemTable smoke test

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod memtable;
+
+use crate::memtable::MemTable;
+
+fn main() {
+    let mut ltsm: MemTable = MemTable::new();
+    ltsm.set(b"hello".to_vec(), b"world".to_vec());
+    println!(
+        "{:?}",
+        ltsm.get(b"hello").map(|v| String::from_utf8_lossy(v))
+    );
+    ltsm.delete(b"hello".to_vec());
+    println!(
+        "{:?}",
+        ltsm.get(b"hello").map(|v| String::from_utf8_lossy(v))
+    );
+}
